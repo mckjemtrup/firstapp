3ec04f2a52b80f14d0b893acb1c5aa118a690813
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-02-21 22:12:52 +0100

    Textbox

--- Swift test/ViewController.swift
+++ Swift test/ViewController.swift
@@ -11,17 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var TheLabel: UILabel!
-    var BtnCount = 0
+    
+    @IBOutlet weak var Textbox1: UITextField!
+    
+    @IBOutlet weak var Textbox2: UITextField!
+    
     @IBAction func btnpushed(_ sender: Any) {
-         TheLabel.text = "/BtnCount"
-        BtnCount = BtnCount + 1
-        print(BtnCount)
-        if(BtnCount >= 10){ print("Stooop")
-        TheLabel.text = "oookay hol it"}
+        print(Textbox1.text!)
+        print(Textbox2.text!)
     }
     
-    @IBAction func buttonpressed(_ sender: Any) {TheLabel.text = "Buttons are pretty cool"
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
