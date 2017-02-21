e9bc1d5b7e1109b7d38734de1fe0677b82cfb6b3
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-02-21 19:37:43 +0100

    Første commit

--- Swift test/ViewController.swift
+++ Swift test/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TheLabel: UILabel!
+    var BtnCount = 0
+    @IBAction func btnpushed(_ sender: Any) {
+         TheLabel.text = "/BtnCount"
+        BtnCount = BtnCount + 1
+        print(BtnCount)
+        if(BtnCount >= 10){ print("Stooop")
+        TheLabel.text = "oookay hol it"}
+    }
+    
+    @IBAction func buttonpressed(_ sender: Any) {TheLabel.text = "Buttons are pretty cool"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
