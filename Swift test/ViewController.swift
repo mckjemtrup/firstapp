ef2c237c8de1259c7d147139b64be4078df32547
Michael Kjemtrup <mckjemtrup@gmail.com>
2017-02-21 19:51:01 +0100

    changed

--- Swift test/ViewController.swift
+++ Swift test/ViewController.swift
@@ -3,7 +3,7 @@
 //  Swift test
 //
 //  Created by Michael Kjemtrup on 21/02/2017.
-//  Copyright © 2017 Michael Kjemtrup. All rights reserved.
+//  Copyright © 2017 Michael Kjemtrup. All rights reserved..
 //
 
 import UIKit
